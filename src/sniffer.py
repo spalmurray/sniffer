97dc1a69105e92eb0e932e6c87f291d445d79831
Spencer Murray <spencer@spalmurray.com>
2022-11-17 16:05:57 -0500

    Extend timeout to 2 seconds. Not scalable but we'll cross that bridge when we get there :P

--- src/sniffer.py
+++ src/sniffer.py
@@ -3,7 +3,7 @@ import requests
 class Sniffer():
     def sniff(self, url: str):
         try:
-            response = requests.get(url, timeout=1)
+            response = requests.get(url, timeout=2)
         except:
             return False
         return response.text
